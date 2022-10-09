60c6335188bd19154f14db6069e8abeceb66060c
Alain Igban <apigban@gmail.com>
2022-10-09 21:47:46 +0400

    added new namespace and pod

--- 01-Network_Policies/pod-creation.sh
+++ 01-Network_Policies/pod-creation.sh
@@ -9,3 +9,9 @@ kubectl expose pod backend --port 80
 # Verify Services and pods
 kubectl get services
 kubectl get pods
+
+# Create New Namespace - Cassandra
+kubectl create cassandra
+
+# Create pod - cassandra
+kubectl run --namespace cassandra cassandra --image=nginx
