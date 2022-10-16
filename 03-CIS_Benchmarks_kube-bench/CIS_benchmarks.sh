d3c5b1b4940fd66c39049417b3a5b72bfe1ef149
Alain Igban <apigban@gmail.com>
2022-10-16 20:08:26 +0400

    run kube-bench installed on master node

--- 03-CIS_Benchmarks_kube-bench/CIS_benchmarks.sh
+++ 03-CIS_Benchmarks_kube-bench/CIS_benchmarks.sh
@@ -3,3 +3,13 @@
 # at the end of the docker command -> --version 1.24
 
 docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -t -v /usr/bin/kubectl:/usr/local/mount-from-host/bin/kubectl docker.io/aquasec/kube-bench:latest --version 1.24
+
+
+# Run kube-bench as installed on master node
+    # see all
+kube-bench run --targets master
+    # or just see the one
+kube-bench run --targets master --check 1.2.25
+
+# Running kubebench using kubernetes as a pod
+# master_bench.yaml
